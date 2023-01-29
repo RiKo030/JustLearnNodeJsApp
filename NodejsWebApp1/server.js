2e42f55912b1def94b48eb7a9739289f53d7f2c9
riko <riko03030303@gmail.com>
2023-01-29 20:07:02 +0200

    css folder

--- NodejsWebApp1/server.js
+++ NodejsWebApp1/server.js
@@ -2,7 +2,12 @@
 const express = require('express');
 const expressHbs = require('express-handlebars');
 const hbs = require("hbs");
+const path = require('path');
 var app = express();
+
+//Public files with acces from browser
+app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(express.static(path.join(__dirname, 'styles')));
 
 app.engine("hbs", expressHbs.engine({
     layoutDir: "views/partials",
@@ -13,7 +18,7 @@ app.set("view engine", "hbs");
 hbs.registerPartials("/views");
 
 var mainController = require('./controllers/mainController.js');
-var userController = require('./controllers/userController.js')
+var userController = require('./controllers/userController.js');
 
 try {
     app.get("/", mainController.main);
