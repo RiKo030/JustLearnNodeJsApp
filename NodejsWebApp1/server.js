0bd5b039fa308d7799af687f9132b88ab22a52d2
riko <riko03030303@gmail.com>
2023-01-30 17:29:26 +0200

    some url fixes

--- NodejsWebApp1/server.js
+++ NodejsWebApp1/server.js
@@ -1,13 +1,31 @@
 'use strict';
+const { error } = require('console');
 const express = require('express');
 const expressHbs = require('express-handlebars');
 const hbs = require("hbs");
 const path = require('path');
+const { Client } = require('pg');
 var app = express();
+
+/*//dbClient with credentials to database
+const client = new Client({
+    host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    password: '12345Forking'
+});
+//Connect to database
+client.connect()
+    .then(() => console.log('Connected'))
+    .catch((err) => console.log('Connection error', err.stack));*/
 
 //Public files with acces from browser
 app.use(express.static(path.join(__dirname, 'public'))); 
 app.use(express.static(path.join(__dirname, 'styles')));
+app.use('/user/regisration', express.static(path.join(__dirname, 'public')));
+app.use('/user/regisration', express.static(path.join(__dirname, 'styles')));
+app.use('/user/account/', express.static(path.join(__dirname, 'public')));
+app.use('/user/account/', express.static(path.join(__dirname, 'styles')));
 
 app.engine("hbs", expressHbs.engine({
     layoutDir: "views/partials",
